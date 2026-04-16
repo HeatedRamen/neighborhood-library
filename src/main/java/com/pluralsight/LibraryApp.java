b3585a509d8b8d222611c67e815927e6a3677b38
ramenmac <kevinnguyen4200@gmail.com>
2026-04-16 15:51:10 -0500

    Create menu function display

--- src/main/java/com/pluralsight/LibraryApp.java
+++ src/main/java/com/pluralsight/LibraryApp.java
@@ -1,9 +1,29 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class LibraryApp {
+    private static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
 
 
+        Book[] bookCollection = new Book[20];
+        boolean isRunning = true;
 
+        while(isRunning){
+            displayHome();
+        }
     }
+    static void displayHome(){
+        println("""
+                ----------------------------------------------------
+                |                                                   |
+                |            (1).  Show Available Books             |
+                |            (2). Show Checked Out Books            |
+                |            (3).         Exit                      |
+                |                                                   |
+                -----------------------------------------------------
+                """);
+    }
+    static void println(String message){System.out.println(message);}
 }
